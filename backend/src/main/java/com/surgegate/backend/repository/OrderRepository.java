9b660b05fb47433a5ea070394c48cf361db0c36f
Aditi <aditisharma9104@gmail.com>
2026-02-04 23:59:43 +0530

    update

--- backend/src/main/java/com/surgegate/backend/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.surgegate.backend.repository;
-
-import com.surgegate.backend.model.Order;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends MongoRepository<Order, String> {
-}
